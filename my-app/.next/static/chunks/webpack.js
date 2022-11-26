b8fee04edc0e368567e70333097662f25e0d776c
Code <code@Decebals-MacBook-Air.local>
2022-11-26 17:39:10 +0100

    Sixth commit

--- my-app/.next/static/chunks/webpack.js
+++ my-app/.next/static/chunks/webpack.js
@@ -57,11 +57,6 @@
 /******/ 	__webpack_require__.i = [];
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	!function() {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	}();
-/******/ 	
 /******/ 	/* webpack/runtime/chunk loaded */
 /******/ 	!function() {
 /******/ 		var deferred = [];
@@ -136,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7b48976abbede96c"; }
+/******/ 		__webpack_require__.h = function() { return "46d0df93c58ab614"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
