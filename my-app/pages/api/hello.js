e722219774440db8af13370d95ceab893b818d55
Code <code@Decebals-MacBook-Air.local>
2022-11-26 17:26:21 +0100

    Forth commit

--- my-app/pages/api/hello.js
+++ my-app/pages/api/hello.js
@@ -1,7 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import type { NextApiRequest, NextApiResponse } from 'next'
-
 export default function handler(
 ) {
   res.status(200).json({ name: 'John Doe' })
